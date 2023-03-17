98c4b910803fa1840bfb674d8df97eec53060442
arasuramanan <arasuaudi@gmail.com>
2023-03-17 07:50:49 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,139 @@
+import styled from "styled-components"
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import RoomIcon from '@mui/icons-material/Room';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+
+const Container = styled.div`
+display: flex;
+`;
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: #d9dddc;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+ 
+const Center = styled.div`
+flex: 1;
+padding: 20px;
+`;
+
+const Right = styled.div`
+flex: 1;
+padding: 20px;
+`;
+
+
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`;
+
+
+const Payment = styled.img`
+    width: 70%;
+`;
+
+
+const Title = styled.h3`
+margin-bottom: 30px;
+`;
+
+
+const List = styled.ul`
+margin: 0;
+padding: 0;
+list-style: none;
+display: flex;
+flex-wrap: wrap;
+`;
+
+
+const ListItem = styled.li`
+width: 50%;
+margin-bottom: 10px;
+`;
+
+
+
+
+const Footer = () => {
+  return (
+    <Container>
+        <Left>
+            <Logo>NANAMAR</Logo>
+            <Desc>Online Shopping Site for Fashion & Lifestyle in India. India's Fashion Expert brings you a variety of footwear, Clothing, Accessories and lifestyle products
+            </Desc>
+            <SocialContainer>
+                <SocialIcon color="3B5999">
+                    <FacebookIcon />
+                </SocialIcon>
+                <SocialIcon color="E4405F">
+                   <InstagramIcon />
+                </SocialIcon>
+                <SocialIcon color="55ACEE">
+                   <TwitterIcon />
+                </SocialIcon>
+                <SocialIcon color="E60023">
+                   <PinterestIcon />
+                </SocialIcon>
+            </SocialContainer>
+          </Left>
+        <Center>
+        <Title>Useful Links</Title>
+        <List>
+        <ListItem>Home</ListItem>
+        <ListItem>Cart</ListItem>
+        <ListItem>Man Fashion</ListItem>
+        <ListItem>Woman Fashion</ListItem>
+        <ListItem>Accessories</ListItem>
+        <ListItem>My Account</ListItem>
+        <ListItem>Order Tracking</ListItem>
+        <ListItem>wishlist</ListItem>
+        <ListItem>Wishlist</ListItem>
+        <ListItem>Terms</ListItem>
+        </List>
+        </Center>
+        <Right>
+        <Title>Contact</Title>
+        <ContactItem><RoomIcon style={{marginRight:"10px"}}/> 70, Central Excise Colony 5th Street Coimbatore</ContactItem>
+        <ContactItem><LocalPhoneIcon style={{marginRight:"10px"}}/>+91 9003613416</ContactItem>
+        <ContactItem><MailIcon style={{marginRight:"10px"}}/>contact@nanamar.com</ContactItem>
+        <Payment src="https://www.citypng.com/public/uploads/preview/-11597193844xtj2mnv37b.png"/>
+        </Right>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "images/micky.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1664431854_5780374.jpg?format=webp&w=376&dpr=1.0",  
+    },
+    {
+      id:2,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1675259764_8079193.jpg?format=webp&w=376&dpr=1.0",
+    },
+    {
+      id:3,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1675922277_5765139.jpg?format=webp&w=376&dpr=1.0",
+    },
+    {
+      id:4,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1674110270_8888184.jpg?format=webp&w=376&dpr=1.0",
+      
+    },
+    {
+      id:5,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/top-selling-tiles-9.jpg?format=webp&w=320&dpr=1.0",
+    },
+    {
+      id:6,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1668491760_8628546.jpg?format=webp&w=376&dpr=1.0",
+    },
+    {
+      id:7,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1663769910_3968515.jpg?format=webp&w=376&dpr=1.0",
+    },
+    {
+      id:8,
+      img:"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1668491940_6649876.jpg?format=webp&w=1080&dpr=1.0",
+    },
+  ]
