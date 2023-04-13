641df749892eb4ae74325af238d7065ec9af91ae
arasuramanan <arasuaudi@gmail.com>
2023-04-13 16:07:03 +0530

    Co-authored-by: Pappu-Shankar <Pappu-Shankar@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,32 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 
 const App = () => {
-  return <Home/>;
+  const user = true
+  return (
+    <>
+    <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+        {/* </Route>
+       <Route path="/success"> 
+        <Success /> */}
+      {/* </Route> */}
+      <Route path="/login" element={<Login />} />{user ? <Navigate to="/" /> :  <Login /> }
+      <Route path="/register" element={<Register  />} />{user ? <Navigate to="/" /> : <Register />}
+      
+    </Routes>
+  </Router>
+  </>
+);
 };
+
+
 
 export default App;
--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
-
 const Container = styled.div`
+
 height: 30px;
 background-color: blue;
 color: white;
